1660ea51112f261ca5225f67b7cad2a52233e276
smjeong <wlarkspur@gmail.com>
2022-07-29 09:02:19 +0000

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+
+const handleHome = (req, res) => {
+  return res.send('middlewares');
+};
+
+
+app.get("/", logger, handleHome);
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT}  🚀`);
+
+app.listen(PORT, handleListening);
+
