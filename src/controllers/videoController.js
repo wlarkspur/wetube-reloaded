6be4de786a5fce37b9db357d1ae2d80f0885b4f7
Seokmin JEONG <38037601+wlarkspur@users.noreply.github.com>
2022-08-02 13:45:23 +0000

    form method="POST" update videos/:id/edit -> update title

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@
       rating: 5,
       comments: 2,
       createdAt: "2 minmute ago",
-      view: 59,
+      views: 1,
       id: 1,
     },
     {
@@ -12,7 +12,7 @@
       rating: 5,
       comments: 2,
       createdAt: "2 minmute ago",
-      view: 59,
+      views: 59,
       id: 2,
     },
     {
@@ -20,7 +20,7 @@
       rating: 5,
       comments: 2,
       createdAt: "2 minmute ago",
-      view: 59,
+      views: 59,
       id: 3,
     },
   ];
@@ -28,13 +28,24 @@ export const trending = (req, res) => {
   return res.render("home", {pageTitle: "Home", videos})
 };
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", {pageTitle:`Watching ${video.title}`})
+  return res.render("watch", {pageTitle:`Watching: ${video.title}`, video})
+}; 
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", {pageTitle:`Editing: ${video.title}`, video});
 };
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video")
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  console.log (req.body)
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
 
