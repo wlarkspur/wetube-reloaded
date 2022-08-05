2b357ba6fa502441bc2c565603c5a0b7a9d05eb5
Your Name <you@example.com>
2022-08-05 15:28:13 +0900

    update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,20 +10,25 @@ console.log("finished")
 
 export const home = async (req, res) => {
   const videos = await Video.find({}); 
-  console.log(videos);
   return res.render("home", {pageTitle: "Home", videos})
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch", {pageTitle:`Watching`})
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", {pageTitle: "Video not found."});
+  }
+  return res.render("watch", {pageTitle: video.title, video});
 }; 
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("edit", {pageTitle:`Editing`});
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", {pageTitle: "Video not found."});
+  }
+  return res.render("edit", {pageTitle: `Edit ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
