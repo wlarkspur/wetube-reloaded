173c10882a4ed9975fd87be74ee419f827425a49
wlarkspur <newacon@naver.com>
2022-08-20 20:33:28 +0200

    last

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -36,7 +36,22 @@ export const postJoin = async (req, res) => {
 };
 
 
-export const getLogin = (req, res) => res.render("login");
+export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
+
+export const postLogin = async (req, res) => {
+    const {username, password} = req.body;
+    const exists = await User.exists({ username });
+    if(!exists) {
+        return res.status(400).render("login", {
+            pageTitle: "Login",
+            errorMessage: "An account with this username does not exists."
+        });
+    };
+    // check if account exists
+    // check if passwoird correct
+    res.end();
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove");
 export const logout = (req, res) => res.send("Log out");
