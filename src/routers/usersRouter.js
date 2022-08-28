c606ae545d74b4adc505227a88d2e6f4d003a535
wlarkspur <newacon@naver.com>
2022-08-28 22:53:47 +0200

    DOCS 0828 +OAUTH GIThub and Fetch Error

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/usersController"
+import { edit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/usersController"
 
 const usersRouter = express.Router();
 
 usersRouter.get("/logout", logout);
 usersRouter.get("/edit", edit);
 usersRouter.get("/remove", remove);
+usersRouter.get("/github/start", startGithubLogin);
+usersRouter.get("/github/finish", finishGithubLogin);
 usersRouter.get("/:id", see);
 
 export default usersRouter;
