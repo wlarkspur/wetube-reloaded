0833241374a916e8f0c8523dc03684436b65e6f4
wlarkspur <newacon@naver.com>
2022-08-30 23:48:47 +0200

    0830 Github Login final

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -42,7 +42,7 @@ export const getLogin = (req, res) => res.render("login", {pageTitle: "Login"});
 
 export const postLogin = async (req, res) => {
     const {username, password} = req.body;
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username, socialOnly: false });
     if(!user) {
         return res.status(400).render("login", {
             pageTitle: "Login",
@@ -100,7 +100,7 @@ export const finishGithubLogin = async (req, res) => {
             },
           })
         ).json();
-          console.log(userData)
+          
         const emailData = await (
             await fetch(`${apiUrl}/user/emails`, {
                 headers: {
@@ -109,18 +109,35 @@ export const finishGithubLogin = async (req, res) => {
               })
         ).json();
           console.log(emailData)
-          const email = emailData.find(
+          const emailObj = emailData.find(
             (email) => email.primary === true && email.verified === true
           );
-          if(!email){
+          if(!emailObj){
             return res.redirect("/login")
           }
+          let user = await User.findOne({ email: emailObj.email });
+          if(!user) {
+              user = await User.create({
+                  name: userData.name,
+                  avatarUrl: userData.avatar_url,
+                  username: userData.login,
+                  email: emailObj.email,
+                  password:"",
+                  socialOnly: true,
+                  location:userData.location,
+              });
+          } 
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect("/");       
     }else {
         return res.redirect("/login")
     }
 };
 
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove");
-export const logout = (req, res) => res.send("Log out");
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { edit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/usersController"
+import { edit,
+         logout,
+         see,
+         startGithubLogin,
+         finishGithubLogin,
+     } from "../controllers/usersController"
 
 const usersRouter = express.Router();
 
 usersRouter.get("/logout", logout);
 usersRouter.get("/edit", edit);
-usersRouter.get("/remove", remove);
 usersRouter.get("/github/start", startGithubLogin);
 usersRouter.get("/github/finish", finishGithubLogin);
 usersRouter.get("/:id", see);
